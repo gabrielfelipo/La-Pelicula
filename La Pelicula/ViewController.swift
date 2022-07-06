0f2352d83e5ccdd33c2a31897a4191ea43d92ace
gabrielfelipo <gfrm@cin.ufpe.br>
2022-07-06 14:18:47 -0300

    first commit

---
+++ La Pelicula/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  La Pelicula
+//
+//  Created by gabrielfelipo on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
